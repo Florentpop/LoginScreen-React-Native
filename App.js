239b3bab66f466594db9085141e70d874f370f99
Florent <florentpop@gmail.com>
2020-11-14 14:56:41 +0000

    Log in Form React Native

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet} from 'react-native';
+import React, { Component } from 'react'
+import LoginScreen from './LoginScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <LoginScreen />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container:{
+    marginTop:100
+  }
+})
---
+++ LoginScreen.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native'
+
+export default class LoginScreen extends Component {
+    render() {
+        return (
+            <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+              <View style={styles.loginTextContainer}>
+              <Text style={styles.loginText}>Log in</Text> 
+              </View> 
+
+              <View>
+                  <TextInput 
+                  style={styles.input} 
+                  placeholderTextColor="#aaaaaa" 
+                  placeholder="Username"/>
+
+                  <TextInput 
+                  style={styles.input} 
+                  placeholderTextColor="#aaaaaa" 
+                  secureTextEntry={true}
+                  placeholder="Password"/>
+                  <Text style={styles.forgotPassword}>Forgot password</Text>
+              </View>
+
+              <View>
+                  <TouchableOpacity style={styles.buttonContainer}>
+                      <Text style={styles.buttonText}>Log in</Text>
+                  </TouchableOpacity>
+              </View>
+
+              <View style={styles.noAccountContainer}>
+                  <Text style={styles.noAccountText}>Dont have an account?</Text>
+                  <Text style={styles.signupText}>Sign up</Text>
+              </View>
+            </ScrollView>
+        ) 
+    }
+}
+
+const styles = StyleSheet.create({
+  container:{
+      marginHorizontal:50
+  },
+
+  loginText:{
+      fontSize:50,
+      color:"#5100ad"
+  },
+
+  loginTextContainer:{
+      marginBottom:30
+  },
+  input:{
+      borderBottomWidth:2,
+      borderBottomColor:"#5100ad",
+      fontSize:20,
+      height:50,
+      marginTop:25 
+  },
+
+  forgotPassword:{
+      alignSelf:"flex-end",
+      marginVertical:3,
+      color:"#0853a8"
+  },
+  buttonContainer:{
+      height:50,
+      backgroundColor:"#5100ad",
+      justifyContent:"center",
+      alignItems:"center",
+      borderRadius:10,
+      marginVertical:70
+      
+  },
+  buttonText:{
+      color:"white",
+      fontSize:25,
+  },
+  noAccountContainer:{
+      flexDirection:"row",
+      justifyContent:"center",
+  },
+  noAccountText:{
+      marginRight:10,
+      fontSize:16
+  },
+
+  signupText:{
+      fontSize:16,
+      color:"#5100ad",
+  }
+})
